e38cf9be57881b8bc476f7a18451740383b0e5ef
Ante Sepic <antesepic@gmail.com>
2018-09-27 14:54:20 +0200

    Update styling

--- components/SelectableList.vue
+++ components/SelectableList.vue
@@ -29,18 +29,65 @@ export default {
         <nuxt-link
         v-if="internalLinks"
         :to="item.url"
-        class="SelectableList-items"
+        class="SelectableList-itemsItemLink js-selectable-list-items-item-link"
         >
+          <div
+          v-if="item.thumb"
+          :style="{
+            'background-image': `url(${item.thumb})`,
+          }"
+          class="SelectableList-itemsItemThumb"
+          />
           {{ item.label }}
         </nuxt-link>
         <a
         v-else
+        class="SelectableList-itemsItemLink"
         href="#"
         @click.prevent="$emit ('selectedItem', item.label)"
         >
+          <div
+          v-if="item.thumb"
+          :style="{
+            'background-image': `url(${item.thumb})`,
+          }"
+          class="SelectableList-itemsItemThumb"
+          />
           {{ item.label }}
         </a>
       </li>
     </ul>
   </div>
 </template>
+
+<style lang="scss">
+.SelectableList-items {
+  border: rem-calc(2) solid #efefef;
+  border-radius: rem-calc(3);
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.SelectableList-itemsItem:not(:last-child) {
+  border-bottom: rem-calc(1) solid #efefef;
+}
+
+.SelectableList-itemsItemLink {
+  align-items: center;
+  color: $secondary-color;
+  display: flex;
+  padding: rem-calc(10);
+  text-decoration: none;
+}
+
+.SelectableList-itemsItemThumb {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: rem-calc(3);
+  height: rem-calc(32);
+  margin-right: rem-calc(15);
+  width: rem-calc(32);
+}
+</style>
